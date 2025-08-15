521da4385f96886028ff30a23749229a776f5e98
Nayanika-Acharjee <nayanikaacharjee@gmail.com>
2025-08-15 14:14:53 +0530

    jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Board from "./Board";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Tic Tac Toe</h1>
+      <Board />
+    </div>
+  );
+}
